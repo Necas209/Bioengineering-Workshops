d60251d47ea568ea9d9ce7e0f1b1a84f0c8686a6
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-10-26 15:12:01 +0100

    .gitignore fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 
 int main() {
-    std::cout << "What's your name?" << std::endl;
+    std::cout << "What's your name? ";
     std::string name;
     std::cin >> name;
     std::cout << "Hello, " << name << "!\n";
+    std::cout << "1 / 0 = " << 1. / 0 << std::endl;
     return 0;
 }
